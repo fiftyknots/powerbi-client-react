39406c4d1b35c1f3060173b32a2070c836863c99
fiftyknots <chaospixel@gmail.com>
2025-07-30 21:26:37 +0200

    Updated index.ts

--- supabase/functions/get-powerbi-embed-config/index.ts
+++ supabase/functions/get-powerbi-embed-config/index.ts
@@ -2,7 +2,7 @@ import { serve } from 'https://deno.land/std@0.168.0/http/server.ts'
 import { verify } from 'https://deno.land/x/djwt@v3.0.1/mod.ts'
 
 const corsHeaders = {
-  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Origin': '*', 
   'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
   'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, PUT, DELETE',
 }
