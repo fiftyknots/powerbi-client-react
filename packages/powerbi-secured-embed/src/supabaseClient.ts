e9620261e69846683b9438dd1f3d89b088b76771
fiftyknots <chaospixel@gmail.com>
2025-07-30 18:42:39 +0200

    Updated supabaseClient.ts

--- packages/powerbi-secured-embed/src/supabaseClient.ts
+++ packages/powerbi-secured-embed/src/supabaseClient.ts
@@ -4,4 +4,6 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL || 'YOUR_SUPABASE_URL';
 const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY || 'YOUR_SUPABASE_ANON_KEY';
 
+console.debug(`SUPABASE_URL: ${supabaseUrl}`);
+console.debug(`SUPABBASE_ANON_KEY: ${supabaseAnonKey}`);
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
